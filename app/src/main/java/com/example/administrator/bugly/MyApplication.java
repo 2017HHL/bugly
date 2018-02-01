663d784fa73cd6fc4a3f6a7993afb495da46f78a
2017HHL <2833638484@qq.com>
2018-02-01 16:23:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/bugly/MyApplication.java
@@ -0,0 +1,35 @@
+package com.example.administrator.bugly;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.beta.Beta;
+
+/**
+ * Created by Administrator on 2018/2/1.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 这里实现SDK初始化，appId替换成你的在Bugly平台申请的appId
+        // 调试时，将第三个参数改为true
+        Bugly.init(this, "9d55543b0a", false);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        // you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+
+        // 安装tinker
+        Beta.installTinker();
+    }
+
+}
